a36c75d009e834ffc69a36872d2ff7288fbe38d6
Yoshi Kamiya <yoshi@sjtu.edu.cn>
2023-02-16 16:13:00 +0800

    Initial Commit

---
+++ SJTU CP2022-23/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SJTU CP2022-23
+//
+//  Created by Yoshi Kamiya on 2023/02/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
