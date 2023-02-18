9e9244e4a4c6ae08ae20d9aadcd06a89d192d260
Yoshi Kamiya <yoshi@sjtu.edu.cn>
2023-02-18 11:04:46 +0800

    Lec #2

--- SJTU CP2022-23/main.cpp
+++ SJTU CP2022-23/main.cpp
@@ -7,8 +7,37 @@
 
 #include <iostream>
 
+class Messenger {
+private:
+	std::string GreetingPhrase;
+	
+public:
+	Messenger() {
+		/* constructor */
+		GreetingPhrase = "Hello";
+		GreetingPhrase2 = "Hello2";
+	}
+	~Messenger() {
+		/* destructor */
+	}
+	
+	void setGreetingPhrase(std::string spec) { GreetingPhrase = spec; };
+	void speak() const {
+		std::cout << GreetingPhrase << ", World!\n";
+	};
+	
+	std::string GreetingPhrase2;
+};
+
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+	std::cout << "Go to ***, World 2!\n";
+	
+	Messenger WhoeverMaybe;
+	WhoeverMaybe.speak();
+	WhoeverMaybe.setGreetingPhrase("Nihao");
+	WhoeverMaybe.speak();
+	std::cout << WhoeverMaybe.GreetingPhrase2 << std::endl;
+	std::cout << WhoeverMaybe.GreetingPhrase << std::endl; /* error */
+	
 	return 0;
 }
