1acfaa225d7581ac05bc601302f35a678646d562
Yoshi Kamiya <yoshi@sjtu.edu.cn>
2023-02-26 21:58:46 +0800

    Lattice.cpp/hpp

---
+++ SJTU CP2022-23/Lattice.cpp
@@ -0,0 +1,8 @@
+//
+//  Lattice.cpp
+//  SJTU CP2022-23
+//
+//  Created by Yoshi Kamiya on 2023/02/26.
+//
+
+#include "Lattice.hpp"
---
+++ SJTU CP2022-23/Lattice.hpp
@@ -0,0 +1,13 @@
+//
+//  Lattice.hpp
+//  SJTU CP2022-23
+//
+//  Created by Yoshi Kamiya on 2023/02/26.
+//
+
+#ifndef Lattice_hpp
+#define Lattice_hpp
+
+#include <stdio.h>
+
+#endif /* Lattice_hpp */
