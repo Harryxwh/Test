b6c44c254e9be48c3513e6a48bb5ba2bbc3574c2
Yoshi Kamiya <yoshi@sjtu.edu.cn>
2023-02-27 10:23:55 +0800

    some tests for NNs in SquareLattice

--- SJTU CP2022-23/UnitTests/Lattice_test.cpp
+++ SJTU CP2022-23/UnitTests/Lattice_test.cpp
@@ -1,7 +1,7 @@
 #include "catch2/catch_amalgamated.hpp"
 #include "Lattice.hpp"
 
-double SquareLattice_N_sites(int L0, int L1) {
+int SquareLattice_N_sites(int L0, int L1) {
 	const int dim = 2;
 	std::vector<int> L(dim);
 	L[0] = L0;
@@ -10,6 +10,19 @@ double SquareLattice_N_sites(int L0, int L1) {
 	return lattice._N_sites();
 }
 
-TEST_CASE("SquareLattice", "test") {
-    REQUIRE(SquareLattice_N_sites(4, 4) == 16);
+int SquareLattice_NN(int L0, int L1, int site_idx, int bond_idx) {
+	const int dim = 2;
+	std::vector<int> L(dim);
+	L[0] = L0;
+	L[1] = L1;
+	SquareLattice lattice(L);
+	return lattice._NN_of_Site(site_idx, bond_idx);
+}
+
+TEST_CASE("SquareLattice") {
+	REQUIRE(SquareLattice_N_sites(4, 4) == 16);
+	REQUIRE(SquareLattice_NN(4, 4, 0, 0) == 1);
+	REQUIRE(SquareLattice_NN(4, 4, 0, 1) == 4);
+	REQUIRE(SquareLattice_NN(4, 4, 0, 2) == 3);
+	REQUIRE(SquareLattice_NN(4, 4, 0, 3) == 12);
 }
